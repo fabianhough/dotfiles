515476e72e43e667465abd93c0aac5038cc5e672
Fabian Hough <self@faba.foo>
2026-03-28 12:37:48 -0400

    Adding tmux config and distro detection

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 
 DOTFILES="$(cd "$(dirname "$0")" && pwd)"
 MINIMAL=false
+DISTRO=
 
 
 # == help ==
@@ -17,10 +18,22 @@ show_help() {
 }
 
 
+# == distro ==
+detect_distro() {
+  if command -v pacman &>/dev/null; then
+    DISTRO="arch"
+  elif command -v apt &>/dev/null; then
+    DISTRO="debian"
+  else
+    echo "Distro unsupported."
+    return 1
+  fi
+}
+
 # == pkg install ==
 install_pkgs() {
   echo "Installing packages..."
-  local packages=(zsh starship neovim zellij)
+  local packages=(zsh starship neovim tmux)
   
   # utilities
   packages+=(rsync eza fzf btop)
@@ -29,9 +42,10 @@ install_pkgs() {
     packages+=(wl-clipboard alacritty ttf-iosevkaterm-nerd ttf-firacode-nerd)
   fi
 
-  if command -v pacman &>/dev/null; then
+  if [[ "$DISTRO" == "arch" ]]; then
+    packages+=(zellij)
     sudo pacman -S --needed "${packages[@]}" || return 1
-  elif command -v apt &>/dev/null; then
+  elif [[ "$DISTRO" == "debian" ]]; then
     sudo apt update && sudo apt install -y "${packages[@]}" || return 1
   else
     echo "Package manager unsupported."
@@ -52,10 +66,18 @@ link_dotfiles() {
   mkdir -p "$HOME/.config/nvim"
   ln -sf "$DOTFILES/nvim/init.lua" "$HOME/.config/nvim/init.lua"
 
-  # zellij
-  mkdir -p "$HOME/.config/zellij"
-  ln -sf "$DOTFILES/zellij/config.kdl" "$HOME/.config/zellij/config.kdl"
+  # tmux
+  mkdir -p "$HOME/.config/tmux"
+  ln -sf "$DOTFILES/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
 
+  # distro specific
+  if [[ "$DISTRO" == "arch" ]]; then
+    # zellij
+    mkdir -p "$HOME/.config/zellij"
+    ln -sf "$DOTFILES/zellij/config.kdl" "$HOME/.config/zellij/config.kdl"
+  fi
+
+  # full or minimal
   if [[ "$MINIMAL" == false ]]; then
     # starship - themed
     ln -sf "$DOTFILES/starship/starship-catppuccin.toml" "$HOME/.config/starship.toml"
@@ -64,6 +86,7 @@ link_dotfiles() {
     mkdir -p "$HOME/.config/alacritty"
     ln -sf "$DOTFILES/alacritty/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
     ln -sf "$DOTFILES/alacritty/color-theme-catppucchin-macchiato.toml" "$HOME/.config/alacritty/color-theme-catppucchin-macchiato.toml"
+
   else
     # starship - baseline
     ln -sf "$DOTFILES/starship/starship.toml" "$HOME/.config/starship.toml"
@@ -84,6 +107,11 @@ done
 # == execute ==
 echo
 echo "Starting dotfile installation..."
+echo
+echo "Checking distro..."
+if ! detect_distro; then
+  exit 1
+fi
 echo
 if ! install_pkgs; then
   echo "Pkg install failed, skipping symlinks"
