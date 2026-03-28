c2bc9a98ca2b5ec44d5058de9003b8634e511875
Fabian Hough <self@faba.foo>
2026-03-27 22:45:47 -0400

    Building install.sh and adding zsh baseline

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+DOTFILES="$(cd "$(dirname "$0")" && pwd)"
+
+
+# == pkg install ==
+install_pkgs() {
+  echo "Installing packages..."
+  local packages=(zsh)
+
+  if command -v pacman &>/dev/null; then
+    sudo pacman -S --needed "${packages[@]}"
+  elif command -v apt &>/dev/null; then
+    sudo apt update && sudo apt install -y "${packages[@]}"
+  else
+    echo "Package manager unsupported."
+    exit 1
+  fi
+}
+
+
+# == symlink ==
+link_dotfiles() {
+  echo "Linking dotfiles..."
+  
+  # zsh
+  ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+
+}
+
+
+# == execute ==
+echo "Starting dotfile installation..."
+echo
+install_pkgs
+echo
+link_dotfiles
+echo
+echo "Complete."
